9fdcb54fafe87fb8058294ca9027503cee192e12
Natalie Somersall <natalie.somersall@gmail.com>
2024-04-03 13:54:04 -0600

    add extra formatting outputs, example of release artifact uploads

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM cgr.dev/chainguard/wolfi-base:latest
 
 # Labels
+LABEL org.opencontainers.image.source https://github.com/some-natalie/bincapz-action
+LABEL org.opencontainers.image.path "Dockerfile"
+LABEL org.opencontainers.image.title "bincapz-action"
+LABEL org.opencontainers.image.description "Run bincapz in a container for GitHub Actions"
+LABEL org.opencontainers.image.authors "Natalie Somersall (@some-natalie)"
+LABEL org.opencontainers.image.licenses "Apache-2.0"
+LABEL org.opencontainers.image.documentation https://github.com/some-natalie/bincapz-action/README.md
 
 # Install the required packages
 RUN apk update && apk add --no-cache \
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,27 @@ else
   cd "$INPUT_WORKDIR" || exit 2
 fi
 
+# sort out input formats
+case "$INPUT_FORMAT" in
+  "json")
+    FILE_EXT="json"
+    ;;
+  "markdown")
+    FILE_EXT="md"
+    ;;
+  "yaml")
+    FILE_EXT="yaml"
+    ;;
+  *)
+    echo "Invalid format provided, using default markdown"
+    INPUT_FORMAT="markdown"
+    FILE_EXT="md"
+    ;;
+esac
+
 # generate report
-echo "Generating report.md" > report.md
+echo "Generating report.md" > bincapz-report."$FILE_EXT"
 find . -type f -not -path '*/\.git/*' | while read -r file; do
      echo "Processing $file"
-     bincapz --format=markdown "$file" >> report.md
+     bincapz --format="$INPUT_FORMAT" "$file" >> bincapz-report."$FILE_EXT"
 done
